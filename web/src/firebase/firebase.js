2af261416a9c7cf72ce91f3ebeb7d0ec90953da2
durham-abric <durham.abric@mail.mcgill.ca>
2018-05-22 20:35:09 -0400

    Basic databse integration complete (firebase).

---
+++ web/src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyCB_dJ67SVUe7-8iRAFH8Ti-bzmxE33XCU",
+    authDomain: "subscrypto-db.firebaseapp.com",
+    databaseURL: "https://subscrypto-db.firebaseio.com",
+    projectId: "subscrypto-db",
+    storageBucket: "subscrypto-db.appspot.com",
+    messagingSenderId: "937720565035"
+  };
+
+export const init=()=>{
+    firebase.initializeApp(config);
+}
+
+export const logout =()=>{
+    firebase.auth().signOut().then(function(){
+    }).catch(()=>alert("Logout Failed"))
+}
