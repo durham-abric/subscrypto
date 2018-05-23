86766c692518f26f6b3719a4fd7ae9499a857bef
durham-abric <durham.abric@mail.mcgill.ca>
2018-05-22 22:07:30 -0400

    Index + Router initially created.

--- web/src/index.js
+++ web/src/index.js
@@ -9,14 +9,45 @@ import {
     Redirect,
     Switch
 } from 'react-router-dom';
+import Navigation from './components/navigation/navbar.js';
+import Footer from './components/navigation/footer.js';
+import About from './components/pages/publicPages/about.js';
+import Login from './components/pages/publicPages/login.js';
+import Signup from './components/pages/publicPages/signup.js';
+import Welcome from './components/pages/publicPages/welcome.js';
+import Profile from './components/pages/privatePages/myProfile.js';
+import RequestService from './components/pages/privatePages/requestService.js';
+import AddAccount from './components/pages/privatePages/addAccount.js';
+import Oops from './components/pages/publicPages/oops.js';
+import * as Firebase from './firebase/firebase.js';
+
+//Configure database connection 
+Firebase.init();
+
+const HandleError = () => {
+    <Redirect to="/oops"/>
+}
 
 ReactDOM.render(
 
-<Router>
-    <App/>
-</Router>
-
-, document.getElementById('root'));
+<div>
+    <Navigation/>
+    <Router>
+        <Switch>
+            <Route exact path="/welcome" component={Welcome}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/signup" component={Signup}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/oops" component={Oops}/>
+            <Route exact path="/*/profile" component={Profile}/>
+            <Route exact path="/*/add-account" component={AddAccount}/>
+            <Route exact path="/*/request-service" component={RequestService}/>
+            <Route path="/*" component={HandleError}/>
+        </Switch>
+    </Router>
+    <Footer/>
+</div>,
+document.getElementById('root'));
 
 
 registerServiceWorker();
