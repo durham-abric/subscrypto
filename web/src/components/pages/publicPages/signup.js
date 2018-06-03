f8c0bc9b3b266f5a0417acba31a91e78091521bb
durham-abric <durham.abric@mail.mcgill.ca>
2018-06-03 00:32:02 -0400

    Signup format complete, needs logic checks & backend

--- web/src/components/pages/publicPages/signup.js
+++ web/src/components/pages/publicPages/signup.js
@@ -1,6 +1,6 @@
 // id=4
 import React, { Component } from 'react';
-import {Panel, Form, FormGroup, ButtonGroup, Button, Row, Radio} from 'react-bootstrap';
+import {Panel, Form, FormGroup, ButtonGroup, Button, Row, Radio, Modal} from 'react-bootstrap';
 //import ProgressBar from '../../subcomponents/progressBar.js'
 
 class Signup extends Component{
@@ -12,9 +12,11 @@ class Signup extends Component{
           progress: 0,
           page: 0,
           userWallet: true,
+          accountCreatedPopup: false,
           newUser:{
             firstName: null,
             lastName: null,
+            dob: null,
             phoneNumber: null,
             email: null,
             password: null,
@@ -27,6 +29,8 @@ class Signup extends Component{
         this.signupPanel = this.signupPanel.bind(this);
         this.handleButton = this.handleButton.bind(this);
         this.backPage = this.backPage.bind(this);
+        this.openPopup = this.openPopup.bind(this);
+        this.closePopup = this.closePopup.bind(this);
       }
       
         resize = () => {
@@ -51,6 +55,7 @@ class Signup extends Component{
           return (
             <div>
               <div className='content-wrapper'>
+                <div id='panel'>
                 <Panel className='signup-panel' style={{marginLeft:'10%', marginRight:'10%'}}>
                   <Panel.Heading><h3> Signup for <b className='subscrypto'> Subscrypto </b></h3></Panel.Heading>
                   <Panel.Body>
@@ -65,6 +70,21 @@ class Signup extends Component{
                     <Button onClick={this.handleButton} style={{height:'34px'}}>{this.state.page<4?<span className="glyphicon glyphicon-chevron-right"/>:<p>Confirm &amp; Create Account</p>}</Button>
                   </Panel.Body>
                 </Panel>
+                </div>
+                <div id='popup'>
+                  <Modal show={this.state.accountCreatedPopup} onHide={this.closePopup}>
+                    <Modal.Header>
+                      <h4>Your account has been created!</h4>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <p>You must verify your account in your email before accessing <b className='subscrypto'>Subscrypto</b>...</p>
+                      <p>Stay tuned for news &amp; updates before the application launches!</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button className='primary' onClick={this.closePopup}>Sounds Good!</Button>
+                    </Modal.Footer>
+                  </Modal>
+                </div>
               </div> 
             </div>);
         }
@@ -77,15 +97,15 @@ class Signup extends Component{
                 <Form>
                   <FormGroup>
                     <label className='control-label'>First Name</label>
-                    <input className='form-control' type='text' placeholder='John'/>
+                    <input className='form-control' type='text' placeholder='John' onChange={(e)=> this.setState({newUser:{...this.state.newUser, firstName: e.target.value}})}/>
                  </FormGroup>
                  <FormGroup>
                     <label className='control-label'>Last Name</label>
-                    <input className='form-control' type='text' placeholder='Doe'/>
+                    <input className='form-control' type='text' placeholder='Doe' onChange={(e)=> this.setState({newUser:{...this.state.newUser, lastName: e.target.value}})}/>
                   </FormGroup>
                   <FormGroup>
                     <label className='control-label'>Date of Birth</label>
-                    <input className='form-control' type='date'/>
+                    <input className='form-control' type='date' onChange={(e)=> this.setState({newUser:{...this.state.newUser, dob: e.target.value}})}/>
                   </FormGroup>
                 </Form>
                 </div>
@@ -97,11 +117,11 @@ class Signup extends Component{
                 <Form>
                   <FormGroup>
                     <label className='control-label'>Email Address</label>
-                    <input className='form-control' type='email' placeholder="johndoe@mail.com"/>
+                    <input className='form-control' type='email' placeholder="johndoe@mail.com" onChange={(e)=> this.setState({newUser:{...this.state.newUser, email: e.target.value}})}/>
                   </FormGroup>
                   <FormGroup>
                     <label className='control-label'>Phone Number</label>
-                    <input className='form-control' type='tel' placeholder='(800) 123-4567'/>
+                    <input className='form-control' type='tel' placeholder='(800) 123-4567' onChange={(e)=> this.setState({newUser:{...this.state.newUser, phoneNumber: e.target.value}})}/>
                     <label style={{fontSize:'10px', color:'gray'}}>Your phone number will be used for account verification before login</label>
                   </FormGroup>
                 </Form>
@@ -118,7 +138,7 @@ class Signup extends Component{
                   </FormGroup>
                   <FormGroup>
                     <label className='control-label'>Confirm Password</label>
-                    <input className='form-control' type='password'/>
+                    <input className='form-control' type='password' onChange={(e)=> this.setState({newUser:{...this.state.newUser, password: e.target.value}})}/>
                     <br/>
                     <label style={{fontSize:'10px', color:'gray'}}>Your password must be 8+ characters &amp; contain at least <u>one</u> numerical digit 
                       &amps; <u>one</u> special character [!, @, #, $, %, &amp;]</label>
@@ -146,20 +166,39 @@ class Signup extends Component{
               );
             break;
             case 4:
+              var name = this.state.newUser.firstName + " " +this.state.newUser.lastName;
+              var birthday = this.state.newUser.dob;
+              var email = this.state.newUser.email;
+              var phone = this.state.newUser.phoneNumber;
+              var password = this.state.newUser.password;
+              var address = this.state.newUser.walletAddress;
               return(
               <div>
                 <Form>
                   <h4> Account Summary </h4>
                   <FormGroup>
+                    <label className='control-label'>Name</label>
+                    <input type="text" readonly className="form-control disabled" value={name}/>          
+                  </FormGroup>
+                  <FormGroup>
+                    <label className='control-label'>Date of Birth</label>
+                    <input type="text" readonly className="form-control disabled" value={birthday}/>          
+                  </FormGroup>
+                  <FormGroup>
                     <label className='control-label'>Email Address</label>
-                    <div class="col-sm-10">
-                      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"/>
-                    </div>                  
+                    <input type="text" readonly className="form-control disabled" value={email}/>          
                   </FormGroup>
                   <FormGroup>
                     <label className='control-label'>Phone Number</label>
-                    <input className='form-control' type='text' placeholder='Doe'/>
-                    <label style={{fontSize:'10px', color:'gray'}}>Your phone number will be used for account verification before login</label>
+                    <input type="text" readonly className="form-control disabled" value={phone}/>          
+                  </FormGroup>
+                  <FormGroup>
+                    <label className='control-label'>Password</label>
+                    <input type="text" readonly className="form-control disabled" value={password}/>          
+                  </FormGroup>
+                  <FormGroup>
+                    <label className='control-label'>Wallet Address</label>
+                    <input type="text" readonly className="form-control disabled" value={address}/>          
                   </FormGroup>
                 </Form>
               </div>
@@ -177,7 +216,7 @@ class Signup extends Component{
               </FormGroup>
               <FormGroup>
                 <label className='control-label'>Confirm Ethereum Wallet Address</label>
-                <input className='form-control' type='text' placeholder='0xAAAAAAAAAAAAAAAAAAAA'/>
+                <input className='form-control' type='text' placeholder='0xAAAAAAAAAAAAAAAAAAAA' onChange={(e)=> this.setState({newUser:{...this.state.newUser, walletAddress: e.target.value}})}/>
                 <label style={{fontSize:'10px', color:'gray'}}><b className='subscrypto'>Subscrypto</b> is NOT responsible for the entry of an incorrect ETH address</label><br/>
                 <label style={{fontSize:'10px', color:'gray'}}><b className='subscrypto'>Subscrypto</b> is NOT responsible for loss of any tokens sent to an incorrect address</label>
               </FormGroup>
@@ -193,10 +232,11 @@ class Signup extends Component{
           var page = this.state.page;
           var progress = this.state.progress;
           if(page < 4){
+            //confirm data has been successfully entered (check state w/ regex expressions)
             this.setState({page: ++page});
             this.setState({progress: progress+=25});
           }else if (page === 4){
-
+            this.createAccount();
           }
           
           
@@ -212,7 +252,7 @@ class Signup extends Component{
         }
 
         createAccount(){
-          alert("Account created!");
+          //Use firebase functions to create the account
           this.setState(
             {
               ...this.state,
@@ -229,6 +269,15 @@ class Signup extends Component{
               }
             }
           );
+          this.openPopup();
+        }
+
+        openPopup(){
+          this.setState({accountCreatedPopup: true});
+        }
+
+        closePopup(){
+          this.setState({accountCreatedPopup: false});
         }
 }
 
